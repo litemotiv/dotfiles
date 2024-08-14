6fd76e05f85d7fad67cd1dd7af6faecd3f52e3e3
Olivier Keun <olivier.keun@gmail.com>
2024-08-14 20:02:43 +0200

    Add sox option with filter

--- home/.local/bin/convert.sh
+++ home/.local/bin/convert.sh
@@ -2,5 +2,6 @@
 mkdir outdir
 
 for i in *.flac; do
-  ffmpeg -i "$i" -sample_fmt s16 -ar 44100 outdir/"$i";
+    ffmpeg -i "$i" -sample_fmt s16 -ar 44100 outdir/"$i";
+#   ffmpeg -i "$i" -af aresample=44100:resampler=soxr:precision=33:osf=s16:dither_method=triangular outdir/"$i";
 done
