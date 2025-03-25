4d4bddf9651def369e04340f9ed57cbca1858ccc
Olivier Keun <olivier.keun@gmail.com>
2025-03-25 15:28:00 +0100

    update

---
+++ home/.config/waybar/scripts/wlsunset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+log="/tmp/wlsunset.log"
+sunrise=`tac $log | grep -om 1 "dawn.*$"`
+temperature=`tac $log | grep -Pom 1 "[\\d]+"`
+echo "{\"text\": \" ${temperature}K\",\"tooltip\": \"${sunrise}\"}"
