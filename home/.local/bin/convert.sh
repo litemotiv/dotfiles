5774f4d8fbd9b039dcf913f119125d46efa95152
Olivier Keun <olivier.keun@gmail.com>
2025-04-15 23:56:24 +0200

    update

--- home/.local/bin/convert.sh
+++ home/.local/bin/convert.sh
@@ -1,7 +1,13 @@
 # Convert to 16/44 FLAC
 mkdir outdir
 
-for i in *.flac; do
-    ffmpeg -i "$i" -sample_fmt s16 -ar 44100 outdir/"$i";
-#   ffmpeg -i "$i" -af aresample=44100:resampler=soxr:precision=33:osf=s16:dither_method=triangular outdir/"$i";
+shopt -s nullglob
+
+for file in *.{flac,wav}; do
+	# assumes only one dot in filename
+	base_name="${file%.*}"
+
+	# convert
+    ffmpeg -i "$file" -sample_fmt s16 -ar 44100 -c:a flac outdir/"${base_name}.flac";
+#   ffmpeg -i "$file" -af aresample=44100:resampler=soxr:precision=33:osf=s16:dither_method=triangular outdir/"${base_name}.flac";
 done
