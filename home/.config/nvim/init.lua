96814c9967eba3043fa82dc269f362b5ff291e50
Olivier Keun <olivier.keun@gmail.com>
2025-12-26 00:06:20 +0100

    'mini.starter'

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,21 +1,33 @@
 require("config.options")
 require("config.mini-deps")
 
-MiniDeps.add('rose-pine/neovim')
-MiniDeps.add('nvim-mini/mini.icons')
-MiniDeps.add('nvim-mini/mini.tabline')
-MiniDeps.add('nvim-mini/mini.statusline')
-MiniDeps.add('nvim-mini/mini.files')
-MiniDeps.add('nvim-mini/mini-git')
+local now, later = MiniDeps.now, MiniDeps.later
 
-vim.cmd("colorscheme rose-pine")
+now(function() 
+	MiniDeps.add('rose-pine/neovim')
+	MiniDeps.add('nvim-mini/mini.icons')
+	MiniDeps.add('nvim-mini/mini.tabline')
+	MiniDeps.add('nvim-mini/mini.statusline')
+	MiniDeps.add('nvim-mini/mini.starter')
 
-require('mini.icons').setup()
-require('mini.tabline').setup()
-require('mini.statusline').setup()
-require('mini.git').setup()
+	vim.cmd("colorscheme rose-pine")
+	require('mini.icons').setup()
+	require('mini.statusline').setup()
+	require('mini.tabline').setup()
+	require('mini.starter').setup({
+		header = "🇳 🇪 🇴 🇻 🇮 🇲",
+		footer = "", 
+	})
+end)
 
-local files = require('mini.files')
-files.setup()
-vim.keymap.set('n', 'E', files.open)
+later(function() 
+	MiniDeps.add('nvim-mini/mini.files')
+	MiniDeps.add('nvim-mini/mini-git')
+
+	require('mini.git').setup()
+
+	local files = require('mini.files')
+	files.setup()
+	vim.keymap.set('n', 'E', files.open)
+end)
 
